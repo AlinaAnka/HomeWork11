07ad50ef38effdb7d19f195fe10e94f98cd4218a
Alina K <alina885@mail.ru>
2025-04-13 19:47:44 +0300

    jenkins test

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -2,12 +2,8 @@ package helpers;
 
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
-import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.Selenide.sessionId;
@@ -35,5 +31,11 @@ public class Attach {
                 "Browser console logs",
                 String.join("\n", Selenide.getWebDriverLogs(BROWSER))
         );
+    }
+
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    public static String addVideo() {
+        return "<html><body><video width='100%' height='100%' controls autoplay>" +
+                "<source src='https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4'></video></body></html>";
     }
 }
