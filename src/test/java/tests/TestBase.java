e2505f29b3ce138f0f5a8e4a7d486c6573ad9cbb
Alina K <alina885@mail.ru>
2025-04-15 22:03:51 +0300

    jenkins test

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,9 +15,9 @@ public class TestBase {
 
     @BeforeAll
     static void setupConfig() {
-        Configuration.browser = System.getProperty("browser");
-        Configuration.browserVersion = System.getProperty("browser_version" );
-        Configuration.browserSize = System.getProperty("screen_resolution");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0" );
+        Configuration.browserSize = System.getProperty("screen_resolution", "1920x1080");
         Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000;
